a1f1f5d0c96d3657bf3505482acd39d416f88404
Legocrocs <163078331+Legocrocs@users.noreply.github.com>
2025-01-23 21:05:22 +0700

    halo bisma

---
+++ HelloBisma.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cout<<"Halo Bisma, Setelah saya pertimbangkan, saya memutuskan untuk memasukkan kamu pada peminatan Computational Data Science. Alasannya karena saya membaca isian yang kamu tulis pada survei peminatan dan saya juga melihat potensi kamu pada peminatan tersebut. Semoga Bisma dapat mempersiapkan diri ya. Semangat!";
+}
