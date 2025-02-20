afdd65c9dc0b4fce42b13624a8f1e6dfde2a443b
Legocrocs <163078331+Legocrocs@users.noreply.github.com>
2025-02-20 09:48:23 +0700

    week update
    bismagemoybismagemoybismagemoybismagemoybismagemoybismagemoybismagemoybismagemoybismagemoybismagemoybismagemoybismagemoybismagemoybismagemoybismagemoybismagemoybismagemoybismagemoybismagemoy

--- calamity-control/scripts/game_manager.gd
+++ calamity-control/scripts/game_manager.gd
@@ -4,7 +4,10 @@ extends Node
 
 var week: int = 1
 
-func ready():
+func _on_end_week_pressed() -> void:
+	next_week()
+
+func _ready():
 	update_week_label()
 
 func next_week():
@@ -12,6 +15,5 @@ func next_week():
 	update_week_label()
 
 func update_week_label():
-	if week_counter:
-		week_counter.text = "Week: " + str (week)
-	print("Week Updated")
+	week_counter.text = "Week: " + str (week)
+	print("Week Updated, Bisma Gemoy, Week: ", week)
