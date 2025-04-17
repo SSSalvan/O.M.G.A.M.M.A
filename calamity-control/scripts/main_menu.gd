99e94670f879f25434570812f59b841a5a58f0ee
Expellar <assasinxros@gmail.com>
2025-04-17 22:54:34 +0700

    Implemented level selection to main menu

--- calamity-control/scripts/main_menu.gd
+++ calamity-control/scripts/main_menu.gd
@@ -10,7 +10,11 @@ func _process(_delta: float) -> void:
 	pass
 
 func _on_play_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main_gameplay.tscn")
+	get_tree().paused = true
+	get_node("/root/LevelDifficultiesSelect/.").visible = true
+	get_node("/root/LevelDifficultiesSelect/level_diff_anim").play("Transition_In_level_diff")
+	get_tree().paused = false
+	
 	
 func _on_exit_game_pressed() -> void:
 	get_tree().quit()
