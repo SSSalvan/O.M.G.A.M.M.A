2602ca668c02169ff645c89ff0735c2338e2eaad
Legorock <jasonjoe.sgia@gmail.com>
2025-04-17 11:38:33 +0700

    shop buy

--- calamity-control/scripts/shop_panel.gd
+++ calamity-control/scripts/shop_panel.gd
@@ -5,26 +5,28 @@ var itemCount: Array[int] = []
 var itemPrice: Array[int] = []
 
 func _ready():
-	itemCount.resize(5)  # now it's [0, 0, 0, 0, 0]
+	itemName.resize(5)  # now it's [0, 0, 0, 0, 0]
+	itemCount.resize(5)
+	itemPrice.resize(5)
 	
-	itemName.append("Panel Surya") #debug
-	itemPrice.append(300)
+	itemName[1]  = "Panel Surya" #debug
+	itemPrice[1] = 300
 	
-	itemName.append("Turbin Angin") #debug, runs at main menu??
-	itemPrice.append(450)
+	itemName[2]  = "Turbin Angin" #debug
+	itemPrice[2] = 450
 	
 	print("Item names array content: ", itemName)
 	#print(itemName[1])
 	#print(itemName[2])
 
 func _on_buy_panel_pressed() -> void:
-	itemCount[1]+=1
 	if ResourceCount.resource >= itemPrice[1]:
+		itemCount[1]+=1
 		ResourceCount.subtract_money(itemPrice[1])
 		print("Bought, now ", itemCount[1], " Panel Surya")
 
 func _on_buy_turbin_pressed() -> void:
-	itemCount[2]+=1
 	if ResourceCount.resource >= itemPrice[2]:
+		itemCount[2]+=1
 		ResourceCount.subtract_money(itemPrice[2])
 		print("Bought, now ", itemCount[2], " Turbin Angin")
