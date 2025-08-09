0500ae02cd0e32eaf60068cc3b6281addc19f7a8
Legorock <jasonjoe.sgia@gmail.com>
2025-08-09 12:44:34 +0700

    a

--- calamity-control/scripts/resource.gd
+++ calamity-control/scripts/resource.gd
@@ -1,9 +1,11 @@
 extends Node
-
+const INT32_MAX = 2147483647
 var resource: int = 1000
 
 func add_money(amount: int) -> void:
 	resource += amount
+	if resource > INT32_MAX:
+		resource = INT32_MAX
 	print("Got money, Resource is now: ", resource)
 
 func subtract_money(amount: int) -> void:
--- calamity-control/unit_testing/test_resourceTesting.gd
+++ calamity-control/unit_testing/test_resourceTesting.gd
@@ -39,3 +39,19 @@ func test_random_amount_end_week_resource_increase():
 		print("Resource after week end: ", ResourceCount.resource)
 		
 		assert_eq(ResourceCount.resource, previousResource + 500, "Resource should be increased by 500")
+
+func test_integer_limit():
+	var game_manager = game_scene.get_node("GameManager")
+	
+	# Simulate a very high value close to the 32-bit signed int limit
+	ResourceCount.resource = 1_000_000_000  # Start high
+	for i in range(4):  # Multiply a few times
+		ResourceCount.resource *= 2  # Must assign the result back
+	
+	var previous_resource = ResourceCount.resource
+	
+	game_manager.weekly_income_resource()
+	
+	var new_resource = ResourceCount.resource
+	assert(new_resource > previous_resource, "Resource did not increase as expected")
+	assert(new_resource <= 2_147_483_647, "Resource exceeded 32-bit integer limit")
